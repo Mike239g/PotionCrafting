8882e3cb22dbf57be67682a8a88c5edfc6cc0f36
ajlares <119090809+Maesunset@users.noreply.github.com>
2025-12-07 23:16:20 -0600

    post prosesing

--- Assets/Scripts/PotionCollisionDetection.cs
+++ Assets/Scripts/PotionCollisionDetection.cs
@@ -1,11 +1,24 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.Serialization;
 
 public class PotionCollisionDetection : MonoBehaviour
 {
     public GameObject liquid;
     public ParticleSystem particleSystem;
+    public Volume globalVolume;
+    public ColorAdjustments colorAdjustments;
+
+    private void Start()
+    {
+        if (globalVolume != null)
+        {
+            globalVolume.profile.TryGet(out colorAdjustments);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Potion"))
@@ -13,7 +26,6 @@ public class PotionCollisionDetection : MonoBehaviour
             Renderer potionRenderer = other.GetComponent<Renderer>();
             AddPotion(potionRenderer);
             Destroy(other.gameObject);
-            
         }
     }
 
@@ -24,5 +36,10 @@ public class PotionCollisionDetection : MonoBehaviour
         main.startColor = potionRenderer.material.color;
         particleSystem.Play();
         renderer.material.color = potionRenderer.material.color;
+        Color temp = renderer.material.color;
+        if (globalVolume != null)
+        {
+            colorAdjustments.colorFilter.value = renderer.material.color;
+        }
     }
 }
