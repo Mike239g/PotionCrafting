2a26c69d99a9d03c2745623d5f0556895bde47fa
ajlares <119090809+Maesunset@users.noreply.github.com>
2026-01-22 16:49:22 -0600

    commit

--- Assets/Scripts/CraftPotion.cs
+++ Assets/Scripts/CraftPotion.cs
@@ -6,8 +6,6 @@ public class CraftPotion : MonoBehaviour
     public Renderer potionRenderer;
     private Color color;
     
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (potionRenderer != null)
